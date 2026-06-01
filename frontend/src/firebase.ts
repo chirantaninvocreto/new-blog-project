3fe6edc1e5c44baf149d16be795b37e66e47f0c0
chirantaninvocreto-ship-it <https://github.com/chirantaninvocreto-ship-it>
2026-06-01 15:09:33 +0530

    added comments in code

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,12 +1,14 @@
+// to use firabase we have to first register our project on the firebase authentication console after creating project the firebaseconfig we get from that website
+// then after adding the project we have to add the authentication provider like email, github ,linkdein 
 import { initializeApp } from "firebase/app";
-
+//this code is taken for sign in with google
 import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
+  getAuth, //to take authorisaton
+  GoogleAuthProvider, //auth provider
+  signInWithPopup, //popup shown after the button click
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  Auth,
+  Auth, //
 } from "firebase/auth";
 
 const firebaseConfig = {
@@ -21,18 +23,16 @@ const firebaseConfig = {
 
 const firebaseapp = initializeApp(firebaseConfig);
 
-const auth = getAuth(firebaseapp);
+const auth = getAuth(firebaseapp); //get auth from the firebase config 
 
 const provider = new GoogleAuthProvider();
 
 const signWithgoogle = async (): Promise<string | undefined> => {
   try {
-    const result = await signInWithPopup(auth, provider);
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const user = result.user;
-    const token = await user.getIdToken();
-    // console.log(token);
-    return token;
+    const result = await signInWithPopup(auth, provider); //in this result is taken from popup by providing auth and provider which declared
+    const user = result.user; 
+    const token = await user.getIdToken();// from user by using getidtoken we can genrate the firbase jwt token which is return to the backend route in the header
+    return token;//retutned token
   } catch (error: any) {
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -48,14 +48,16 @@ const signWithgoogle = async (): Promise<string | undefined> => {
 
 export { auth, provider, signWithgoogle };
 
+
+// to add the user with user email id and password in this password is not stored ot get stored to firebase auth
 let signwithEmail = async (email: string, password: string) => {
   try {
     let userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password,
+      auth,  // asking which auth provider is used or using the auth form firebase config
+      email,// given by user
+      password,//given by user
     );
-    let user = userCredential.user;
+    let user = userCredential.user;// usercredential gives big object form that object takes only user part
     let token = await user.getIdToken();
     console.log("token:", token);
     return token;
@@ -64,12 +66,12 @@ let signwithEmail = async (email: string, password: string) => {
     console.log("Firebase error message:", error.message);
   }
 };
-
+//this signwithuser is used to sign in the user which had made account by using the create eamil using email and passowrd 
 let signwithuser = async (auth: Auth, email: string, password: string) => {
   let userCredential = await signInWithEmailAndPassword(auth, email, password);
   let user = userCredential.user;
   let token = await user.getIdToken();
-  return token
+  return token;
 };
 
 export { signwithEmail, signwithuser, Auth };
